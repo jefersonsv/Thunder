d96de4c50d448168af04b3f00ee24bd5dae4cd67
Jeferson Tenorio <jefersonsv@gmail.com>
2018-06-15 16:19:53 -0300

    fix template

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 Thunder web framework where productivity and performance go together
 
 # Getting Started
+Install template once time using the command
 ```
-dotnet new -i Thunder.Web.Framework
+dotnet new -i Thunder.Web.Framework.Template
 ```
+Create new web application
+```
+dotnet new thunder
+```
+
 # Simple usage
 The simple usage configure a custom route **/hello** and render *.html inside all directories of **.\wwwroot**
 
--- nuget-pack.cmd
+++ nuget-pack.cmd
@@ -1,3 +1,9 @@
+REM https://docs.microsoft.com/pt-br/dotnet/core/tools/custom-templates
 rd /s /q .\src\Thunder\bin
 rd /s /q .\src\Thunder\obj
-nuget pack .\src\Thunder\Thunder.nuspec
+del /f /s /q *.nupkg
+nuget pack .\src\Thunder\Thunder.Web.Framework.nuspec
+nuget push *.nupkg -Source https://api.nuget.org/v3/index.json
+del /f /s /q *.nupkg
+nuget pack .\src\Thunder\Thunder.Web.Framework.Template.nuspec
+nuget push *.nupkg -Source https://api.nuget.org/v3/index.json
