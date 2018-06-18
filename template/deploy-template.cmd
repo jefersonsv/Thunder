b6e0ec759e3996c6b3d7b3fa1ca0578be85d4d59
Jeferson Tenorio <jefersonsv@gmail.com>
2018-06-18 13:49:35 -0300

    update readme

--- template/deploy-template.cmd
+++ template/deploy-template.cmd
@@ -1,4 +1,5 @@
 REM https://docs.microsoft.com/pt-br/dotnet/core/tools/custom-templates
-nuget pack .\template\Startup\Thunder.Web.Framework.Template.nuspec
+del /f /s /q *.nupkg
+nuget pack .\Thunder.Web.Framework.Template.nuspec
 nuget push *.nupkg -Source https://api.nuget.org/v3/index.json
 del /f /s /q *.nupkg
